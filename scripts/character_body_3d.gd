99adb4540f97e53f591b892af5ee97f849fefe1c
Noah <noahc214@hotmail.com>
2024-08-26 17:04:56 -0700

    added more assets, game 90% complete

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,7 +1,8 @@
 class_name Player extends CharacterBody3D
 
 @export_category("Player")
-@export_range(1, 35, 1) var speed: float = 10 # m/s
+@export_range(1, 35, 1) var walk_speed: float = 10 # m/s
+@export_range(1, 35, 1) var sprint_speed: float = 10 # m/s
 @export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
 
 @export_range(0.1, 3.0, 0.1) var jump_height: float = 1 # m
@@ -10,6 +11,8 @@ class_name Player extends CharacterBody3D
 var jumping: bool = false
 var mouse_captured: bool = false
 
+var speed: float = walk_speed
+var stamina: float = 3
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var move_dir: Vector2 # Input direction for movement
@@ -20,6 +23,10 @@ var grav_vel: Vector3 # Gravity velocity
 var jump_vel: Vector3 # Jumping velocity
 
 var lantern_interact = false
+var sleep = false
+var whispers_started = false
+
+@onready var effect_anim = $EffectAnim
 
 @onready var camera: Camera3D = $Camera3D
 @onready var player_arms: Node3D = $Player_Arms
@@ -29,12 +36,39 @@ var lantern_interact = false
 @onready var zone_2: Area3D = $"../Whisper Zones/Zone2"
 @onready var zone_3: Area3D = $"../Whisper Zones/Zone3"
 
+# Dead person audio
+@onready var dead_audio = $"../AnimalBody/AudioStreamPlayer3D"
+
+# Whispers audio
+@onready var whispers = $Whispers
+
 # Lantern
-@onready var lantern: MeshInstance3D = $"../cryptid_camping/Farol"
+@onready var lantern: MeshInstance3D = $"../cryptid_camping/Campsite/Farol"
 
+@onready var label = $CanvasLayer/InteractLabel
+@onready var dialogue = $CanvasLayer/DialogueLabel
+@onready var color_rect = $CanvasLayer/ColorRect
 
+@onready var death_animation = $"Taken Hostage - Villain/AnimationPlayer"
+@onready var demon = $"Taken Hostage - Villain"
+@onready var animal_body = $"../AnimalBody"
+@onready var death = $Death
+@onready var ambient_noise = $"../AudioStreamers/AmbientNoise"
+@onready var footstep1 = $Footstep1
+@onready var footstep2 = $Footstep2
+
+var bus_index: int
+
+var step_toggle = false
+
+var elapsed_time := 0.0
+var target_time = 0.7
 
 func _ready() -> void:
+	set_process_input(false)
+	bus_index = AudioServer.get_bus_index(whispers.bus)
+	speed = walk_speed
+	color_rect.visible = false
 	capture_mouse()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -45,12 +79,74 @@ func _unhandled_input(event: InputEvent) -> void:
 	#if Input.is_action_just_pressed("exit"): get_tree().quit()
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_released("sprint"):
+		speed = walk_speed
+	
+	stamina += delta * 0.5 if stamina < 3 else 0
+	
+	if stamina <= 0:
+		speed = walk_speed
+		await get_tree().create_timer(3).timeout
+	else:
+		if Input.is_action_pressed("sprint"):
+			speed = sprint_speed
+			stamina -= delta 
+		
+	if not whispers_started and Main.whispers:
+		
+		whispers.play()
+		whispers_started = true
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		set_process_input(false)
+		
+		death_animation.play("mixamo_com")
+		await get_tree().create_timer(1).timeout
+		death.play()
+		await get_tree().create_timer(1.7).timeout
+		effect_anim.play("death")
+
+		
 	if Input.is_action_just_pressed("interact") and lantern_interact and lantern.visible:
 		lantern.queue_free()
 		player_arms.visible = true
+	
+	if Input.is_action_just_pressed("interact") and Main.sleepable and sleep:
+		set_process_input(false)
+		color_rect.visible = true
+		Main.section += 1
+		Main.sleepable = false
+		label.text = ""
+		effect_anim.play("sleep")
+		await get_tree().create_timer(5).timeout
+		dead_audio.play()
+		await get_tree().create_timer(2).timeout
+		effect_anim.play("wake")
+		await get_tree().create_timer(0.2).timeout
+		color_rect.visible = false
+		dialogue.text = "What the hell?"
+		set_process_input(true)
+		await get_tree().create_timer(2).timeout
+		dialogue.text = ""
+		await get_tree().create_timer(0.2).timeout
+		dialogue.text = "* Go investigate the source of the scream"
+		await get_tree().create_timer(2).timeout
+		dialogue.text = ""
 
+		
 func _physics_process(delta: float) -> void:
 	velocity = _walk(delta) + _gravity(delta) 
+	if velocity != Vector3.ZERO:
+		
+		elapsed_time += delta
+		if elapsed_time >= target_time:
+			step_toggle = !step_toggle
+			if step_toggle:
+				footstep1.play()
+			else:
+				footstep2.play()
+			elapsed_time = 0
+		
 	move_and_slide()
 
 func capture_mouse() -> void:
@@ -62,12 +158,8 @@ func release_mouse() -> void:
 	mouse_captured = false
 
 func _rotate_camera(sens_mod: float = 1.0) -> void:
-	
-	camera.rotation.y -= look_dir.x * camera_sens * sens_mod
+	rotation.y -= look_dir.x * camera_sens * sens_mod
 	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
-	player_arms.rotation.y -= look_dir.x * camera_sens * sens_mod
-
-
 
 func _walk(delta: float) -> Vector3:
 	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
@@ -82,19 +174,23 @@ func _gravity(delta: float) -> Vector3:
 
 # tent for sleeping
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and Main.sleepable:
+		label.text = "Sleep"
+		sleep = true
 		print("Wants to sleep")
 	pass # Replace with function body.
 
-# dead animal
-func _on_animal_body_body_entered(body: Node3D) -> void:
+# tent move away
+func _on_area_3d_body_exited(body):
 	if body.name == "Player":
-		print("Sees dead animal")
-	pass # Replace with function body.
+		label.text = ""
+
 
 
 func _on_zone_1_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		var current_db = AudioServer.get_bus_volume_db(bus_index)
+		AudioServer.set_bus_volume_db(bus_index, current_db + 6.0)
 		print("Zone 1 passed, increased whispers")
 		zone_1.queue_free()
 	pass # Replace with function body.
@@ -102,26 +198,48 @@ func _on_zone_1_body_entered(body: Node3D) -> void:
 
 func _on_zone_2_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		var current_db = AudioServer.get_bus_volume_db(bus_index)
+		AudioServer.set_bus_volume_db(bus_index, current_db + 6.0)
 		print("Zone 2 passed, increased whispers")
-		zone_1.queue_free()
+		zone_2.queue_free()
 	pass # Replace with function body.
 
 
 func _on_zone_3_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		whispers.stop()
+		ambient_noise.stop()
 		print("Zone 3 passed, stopped whispers")
-		zone_1.queue_free()
+		zone_3.queue_free()
 	pass # Replace with function body.
 
 # handle lantern pickup
 func _on_lantern_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and Main.lantern_interactable:
 		print("wants to pick up lantern")
 		lantern_interact = true
-		
-	pass # Replace with function body.
+		label.text = "Pickup"
+
 
 
 func _on_lantern_body_exited(body: Node3D) -> void:
 	if body.name == "Player":
 		lantern_interact = false
+		label.text = ""
+
+
+func _on_cutscene_animation_finished(anim_name):
+	set_process_input(true)
+	dialogue.text = "I really should get to bed... it's late"
+	await get_tree().create_timer(3).timeout
+	dialogue.text = ""
+
+
+func _on_scratched_van_body_entered(body):
+	if body.name == "Player":
+		dialogue.text = "What the fuck is going on!?"
+		await get_tree().create_timer(2).timeout
+		dialogue.text = "There is a ranger tower up the road, I need to get help!"
+		await get_tree().create_timer(3).timeout
+		dialogue.text = ""
+	
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,126 @@
 extends Node3D
+class_name Main
+
+@onready var cut_scene_cam = $SubViewportContainer/SubViewport/CutsceneVan/CutSceneCam
+@onready var cutscene_van = $SubViewportContainer/SubViewport/CutsceneVan
+@onready var parked_van_clean = $SubViewportContainer/SubViewport/ParkedVan_Clean
+@onready var player = $SubViewportContainer/SubViewport/Player
+@onready var campsite = $SubViewportContainer/SubViewport/cryptid_camping/Campsite
+@onready var ambient_noise = $SubViewportContainer/SubViewport/AudioStreamers/AmbientNoise
+@onready var deer = $SubViewportContainer/SubViewport/deer_1
+@onready var camp_boundary = $SubViewportContainer/SubViewport/Boundaries/CampBoundaries/Camp
+@onready var dead_boundary = $SubViewportContainer/SubViewport/Boundaries/CampBoundaries/DeadBody
+@onready var deadman = $SubViewportContainer/SubViewport/deadman
+@onready var animal_body = $SubViewportContainer/SubViewport/AnimalBody
+@onready var car_scratched = $SubViewportContainer/SubViewport/Car4Scratched
+@onready var car_alarm = $SubViewportContainer/SubViewport/Car4Scratched/AudioStreamPlayer3D
+@onready var scratched_van = $SubViewportContainer/SubViewport/Car4Scratched/ScratchedVan
+@onready var scratched_van_col = $SubViewportContainer/SubViewport/Car4Scratched/ScratchedVan/ScratchedVan_col
+
+@onready var left_light = $SubViewportContainer/SubViewport/Car4Scratched/SpotLight3D
+@onready var left_bulb = $SubViewportContainer/SubViewport/Car4Scratched/SpotLight3D/MeshInstance3D
+@onready var right_light = $SubViewportContainer/SubViewport/Car4Scratched/SpotLight3D2
+@onready var right_bulb = $SubViewportContainer/SubViewport/Car4Scratched/SpotLight3D2/MeshInstance3D2
+
+static var section = 0
+static var sleepable = false
+static var lantern_interactable = false
+static var whispers = false
+var rotation_speed: float = 3.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	scratched_van_col.disabled = true
+	player.visible = false
+	car_scratched.visible = false
+	deadman.visible = false
+	camp_boundary.visible = true
+	dead_boundary.visible = false
+	cut_scene_cam.current = true
+	ambient_noise.playing = true
 
+var time_passed := 0.0
+var interval := 1.0
+var light_state = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction_to_player = (player.global_position - deer.global_position).normalized()
+	direction_to_player.y = 0
+	var target_rotation = direction_to_player.angle_to(Vector3.LEFT)
+	deer.rotation.y = lerp_angle(deer.rotation.y, -target_rotation, rotation_speed * delta)
+	# getting out of van and making campsite appear
+	if section == 1:
+		print(str(section))
+		for child in campsite.get_children():
+			child.visible = true
+		section += 1
+		
+	# allowing for sleeping in tent. camp gets closed in
+	if section == 2:
+		print(str(section))
+		sleepable = true
+		
+	# investigate scream
+	if section == 3:
+		print(str(section))
+		if camp_boundary:
+			camp_boundary.queue_free()
+			camp_boundary = null
+		dead_boundary.visible = true
+		sleepable = false
+		lantern_interactable = true
+		# show blood
+		# reveal dead body
+		deadman.visible = true
+		# relocate deer
+		deer.position = Vector3(0.018, -4.169, -11.657)
+	if section == 4:
+		
+		parked_van_clean.visible = false
+		car_scratched.visible = true
+		scratched_van_col.disabled = false
+		time_passed += delta
+		if time_passed >= interval:
+			toggle_lights()
+			time_passed = 0.0
+	if section == 5:
+		whispers = true
+		pass
+
+func toggle_lights():
+	if light_state:
+		left_light.visible = false
+		right_light.visible = false
+		light_state = false
+	else:
+		left_light.visible = true
+		right_light.visible = true
+		light_state = true
+
+func _on_animal_body_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		print("sees dead body")
+		car_alarm.play()
+		animal_body.queue_free()
+		section += 1
+	pass # Replace with function body.
+
+func _on_animation_player_animation_finished(anim_name):
+	print("intro finsihed")
+	cut_scene_cam.current = false
+	cutscene_van.visible = false
+	parked_van_clean.visible = true
+	player.visible = true
+	section += 1
+
+
+func _on_scratched_van_body_entered(body):
+	if body.name == "Player":
+		print("sees car")
+		whispers = true
+		car_alarm.stop()
+		scratched_van.queue_free()
+		section += 1
+		dead_boundary.queue_free()
